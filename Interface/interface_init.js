e31e9123047f99bea68d009ee07928d796221f91
avhishek <abishek.paudel@swifttech.com.np>
2022-09-20 11:15:49 +0545

    bug fix

--- Interface/interface_init.js
+++ Interface/interface_init.js
@@ -1,5 +1,6 @@
-function Print(labeledObj) {
-    console.log(labeledObj.label);
-}
-var myObj = { size: 10, label: "Size 10 Object" };
-Print(myObj);
+// function Print(labeledObj: { label: string }) {
+//     console.log(labeledObj.label);
+//   }
+var kv1 = { key: 1, value: "steve" };
+// let kv2: KeyPair ={ key:1,value:"steve"};
+// let kv3: keypair ={key:1,value:100}
--- Interface/interface_init.ts
+++ Interface/interface_init.ts
@@ -4,3 +4,24 @@ function Print(labeledObj: { label: string }) {
    
   let myObj = { size: 10, label: "Size 10 Object" };
   Print(myObj);
+
+  // example of interface
+  interface Employee 
+  {
+    empcode:number;
+    empname:string;
+    getsalary:(number)=>number;
+    getManagerNAme(number):string;
+  }
+
+
+  interface KeyPair
+  {
+    key:number;
+    value:string;
+
+  }
+
+  let kv1: KeyPair ={ key:1,value:"steve"};
+  // let kv2: KeyPair ={ key:1,value:"steve"};
+  // let kv3: keypair ={key:1,value:100}
